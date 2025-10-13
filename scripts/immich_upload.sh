e722da16f93df6554123e321437c139a4c824090
Fritz Duchardt <fritz@duchardt.net>
2025-10-13 14:53:23 +0200

    fix: fix immich

--- scripts/immich_upload.sh
+++ scripts/immich_upload.sh
@@ -4,4 +4,3 @@ export IMMICH_API_KEY="${1:?Provide Immich API Key}"
 export IMMICH_INSTANCE_URL="${2:-https://immich.duchardt.net/api}"
 docker run --network host -it -v "$(pwd)":/import:ro -e IMMICH_INSTANCE_URL -e IMMICH_API_KEY \
   ghcr.io/immich-app/immich-cli:latest upload --recursive -a /import
-w
