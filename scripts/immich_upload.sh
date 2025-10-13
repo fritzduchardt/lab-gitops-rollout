e4a5f6ae2a0eeefa08e9ae9cb9d5b90ea0f589f2
Fritz Duchardt <fritz@duchardt.net>
2025-10-13 13:39:29 +0200

    fix: fix immich script

--- scripts/immich_upload.sh
+++ scripts/immich_upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export IMMICH_API_KEY="${1:?Provide Immich API Key}"
-export IMMICH_INSTANCE_URL="${2:-https://immich-friclu.duckdns.org/api}"
+export IMMICH_INSTANCE_URL="${2:-https://immich.duchardt.net/api}"
 docker run --network host -it -v "$(pwd)":/import:ro -e IMMICH_INSTANCE_URL -e IMMICH_API_KEY \
   ghcr.io/immich-app/immich-cli:latest upload --recursive -a /import
 w
